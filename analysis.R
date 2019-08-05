713e7962033a05a5cc32a8d3caa6400d7250a993
vmshirey <vms55@georgetown.edu>
2019-08-05 19:57:05 -0400

    - updated code
    added .gitignore

---
+++ analysis.R
@@ -0,0 +1,22 @@
+# Obtains current occurrence data in GBIF for a taxonomic list of spiders and two .csv files
+# containing literature based records for spiders of the Iberian Penninsula and elsewhere.
+# Calculates extent of occurrence (an IUCN metric for Red Lists) using these records and compares the putative
+# IUCN Red List designations of the litereture, GBIF, and combined datasets.
+#
+# Author: Vaughn M. Shirey
+
+# load required libraries
+library(red); library(reshape2)
+
+# load and extract unique taxa from Iberian dataset
+idat <- read.csv("iberian_occur.txt", sep = "\t", header = TRUE)
+itaxa <- as.vector(unique(idat$scientificName))
+
+# load and extract unique taxa from global dataset
+gdat <- read.csv("srli_occur.txt", sep = "\t", header = TRUE)
+gtaxa <- as.vector(unique(gdat$scientificName))
+
+# grab GBIF records for all unique taxa in the Iberian dataset
+
+# grab GBIF records for all unique taxa in the global dataset
+
