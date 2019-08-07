d2bb58c20b074c44a8da25499a10c062e71da3e3
vmshirey <vms55@georgetown.edu>
2019-08-07 14:51:51 -0400

    Update analysis.R

--- analysis.R
+++ analysis.R
@@ -8,28 +8,113 @@
 # load required libraries
 library(red); library(reshape2)
 
-# load and extract unique taxa from Iberian dataset
+# load Iberian dataset
 idat <- read.csv("iberian_occur.txt", sep = "\t", header = TRUE)
-itaxa <- as.vector(unique(idat$scientificName))
 
-# load and extract unique taxa from global dataset
+# load global dataset
 gdat <- read.csv("srli_occur.txt", sep = "\t", header = TRUE)
-gtaxa <- as.vector(unique(gdat$scientificName))
+
+# reformat both Iberian and global literature data to match Red-package GBIF output, grab unique taxa
+idat <- (data.frame(long = idat[, 17], lat = idat[, 16], V1 = idat[, 19]))
+gdat <- (data.frame(long = gdat[, 9], lat = gdat[, 8], V1 = gdat[, 13]))
+idat <- idat[!is.na(idat$long),]
+gdat <- gdat[!is.na(gdat$long),]
+
+itaxa <- as.vector(unique(idat[, 3]))
+gtaxa <- as.vector(unique(gdat[, 3]))
 
 # grab GBIF records for all unique taxa in the Iberian dataset, remove records from contributed literature dataset
 n <- length(itaxa)
 temp <- data.frame()
-dat <- list()
+idat_gbif <- list()
 
 for(i in 1:n){
-  taxon <- itaxa[i] ## grab names
+  taxon <- itaxa[i]
+  
+  print(paste("Processing: ", i, " of ", n, "."))
   
   temp <- data.frame()   
-  try(temp <- records(taxon))  ## try getting records from GBIF
+  try(temp <- records(taxon))
   try(temp$V1 <- taxon)
   
-  dat[[i]] <- temp
+  idat_gbif [[i]] <- temp
 }
 
-# grab GBIF records for all unique taxa in the global dataset, remove records from contributed literature dataset
+idat_gbif = do.call(rbind, idat_gbif) 
+idat_gbif <- unique(idat_gbif) 
+idat_gbif <- idat_gbif[!(idat_gbif %in% idat),]
 
+# grab GBIF records for all unique taxa in the global dataset, remove records from contributed literature dataset
+n <- length(gtaxa)
+temp <- data.frame()
+gdat_gbif <- list()
+
+for(i in 1:n){
+  taxon <- gtaxa[i]
+  
+  print(paste("Processing: ", i, " of ", n, "."))
+  
+  temp <- data.frame()   
+  try(temp <- records(taxon))
+  try(temp$V1 <- taxon)
+  
+  gdat_gbif[[i]] <- temp
+}
+
+gdat_gbif = do.call(rbind, gdat_gbif)
+gdat_gbif <- unique(gdat_gbif)
+gdat_gbif <- gdat_gbif[!(gdat_gbif %in% gdat),]
+
+# calculate EOO from literature only data set for both Iberian and global lists
+n <- length(itaxa)
+idat_eoo <- data.frame()
+
+for(i in 1:n){
+  taxon <- itaxa[i]
+  
+  print(paste("Calculating: ", i, " of ", n, "."))
+  
+  idat_eoo[i, 1] <- taxon
+  idat_eoo[i, 2] <- eoo(idat[which(idat[, 3]==taxon), c(1,2)])
+}
+
+n <- length(gtaxa)
+gdat_eoo <- data.frame()
+
+for(i in 1:n){
+  taxon <- gtaxa[i]
+  
+  print(paste("Calculating: ", i, " of ", n, "."))
+  
+  gdat_eoo[i, 1] <- taxon
+  gdat_eoo[i, 2] <- eoo(gdat[which(gdat[, 3]==taxon), c(1,2)])
+}
+
+# calculate EOO from GBIF only dataset for both Iberian and global lists
+n <- length(itaxa)
+idat_gbif_eoo <- data.frame()
+
+for(i in 1:n){
+  taxon <- itaxa[i]
+  
+  print(paste("Calculating: ", i, " of ", n, "."))
+  
+  idat_gbif_eoo[i, 1] <- taxon
+  idat_gbif_eoo[i, 2] <- eoo(idat_gbif[which(idat_gbif[, 3]==taxon), c(1,2)])
+}
+
+n <- length(gtaxa)
+gdat_gbif_eoo <- data.frame()
+
+for(i in 1:n){
+  taxon <- gtaxa[i]
+  
+  print(paste("Calculating: ", i, " of ", n, "."))
+  
+  gdat_gbif_eoo[i, 1] <- taxon
+  gdat_gbif_eoo[i, 2] <- eoo(gdat_gbif[which(gdat_gbif[, 3]==taxon), c(1,2)])
+}
+
+# calculate EOO for the combined dataset for both Iberian and global lists
+idat_merge <- rbind(idat, idat_gbif)
+gdat_merge <- rbind(gdat, gdat_gbif)
